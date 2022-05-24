8db9b0b1a59ff14dab44cc747ff2f54c195c34fc
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-05-24 08:08:58 -0300

    Initial commit

---
+++ Assets/ObjectMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+
+    public GameObject myCube;
+
+    public Vector3 scaleVector;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        myCube.transform.localScale += scaleVector;
+        //myCube.transform.position += movementVector;
+        //myCube.transform.eulerAngles += rotationVector;
+    }
+}
